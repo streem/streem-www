89fefa429d0c5a6a29004575133061f467524acc
Shawn Lehner <shawnlehner@gmail.com>
2019-02-23 14:19:28 -0800

    Added floaters and lots of polish

--- src/js/custom/modal.js
+++ src/js/custom/modal.js
@@ -1,11 +1,13 @@
 $(function() {
     $('body')
         .on('click', '[data-modal-close]', function() {
-            $(this).closest('.modal').fadeOut();
+            $(this).closest('.modal').fadeOut(function() { $(this).find('.animate').removeClass('reveal'); });
             return false;
         })
         .on('click', '[data-modal-show]', function() {
-            $($(this).data('modal-show')).addClass('show').hide().fadeIn();
+            $($(this).data('modal-show')).addClass('show').hide().fadeIn(function() {
+                $(this).find('.animate').addClass('reveal');
+            });
             return false;
         });
 });
