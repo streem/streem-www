b4189af87f43dd03ec59ec0a7ad62ca08a915796
Shawn Lehner <shawnlehner@gmail.com>
2020-05-27 16:15:04 -0700

    Implemented new hubspot form and styled and updated footer contact info

--- src/js/custom/modal.js
+++ src/js/custom/modal.js
@@ -1,13 +1,44 @@
-window.showModal = function($modal) {
-    $modal.addClass('show').hide().fadeIn(function() {
-        $(this).find('.animate').addClass('reveal');
-    });
-}
+(function() {
+    var html = document.getElementsByTagName('html')[0];
+    var locked = false;
+    var offsetMemory = 0;
+
+    window.showModal = function($modal) {
+        $modal.addClass('show').hide().fadeIn(function() {
+            $(this).find('.animate').addClass('reveal');
+
+            if (locked) return;
+
+            html.style.overflowY = 'hidden';
+            offsetMemory = window.pageYOffset || document.documentElement.scrollTop;
+            locked = true;
+        });
+    }
+
+    window.closeModal = function($modal) {
+        $modal.fadeOut(function() { 
+            $(this).find('.animate').removeClass('reveal'); 
+        });
+
+        html.style.overflowY = 'auto';
+        locked = false;
+
+        setTimeout(function() {
+            window.scrollTo(0, offsetMemory);
+        }, 10);
+    }
+
+})();
+
+
 
 $(function() {
+
+    
+
     $('body')
         .on('click', '[data-modal-close]', function() {
-            $(this).closest('.modal').fadeOut(function() { $(this).find('.animate').removeClass('reveal'); });
+            closeModal($(this).closest('.modal'));
             return false;
         })
         .on('click', '[data-modal-show]', function() {
